e9e35f08e9aa033773d203779fe526f08f7446e0
Tuhin Poddar <tuhinpoddar114@gmail.com>
2024-09-28 19:32:30 +0530

    dragable notes function

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,12 +1,17 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Trash from "../icons/Trash";
 
 const NoteCard = ({ note }) => {
   const colors = JSON.parse(note.colors);
   const body = JSON.parse(note.body);
-  const position = JSON.parse(note.position);
+  //   const position = JSON.parse(note.position);
+  const [position, setPositon] = useState(JSON.parse(note.position));
 
   const textAreaRef = useRef(null);
+
+  let mouseStartPos = { x: 0, y: 0 };
+
+  const cardRef = useRef(null);
 
   useEffect(() => {
     autoGrow(textAreaRef);
@@ -18,8 +23,40 @@ const NoteCard = ({ note }) => {
     current.style.height = current.scrollHeight + "px"; // Set the new height
   };
 
+  const mouseDown = (e) => {
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    document.addEventListener("mousemove", mouseMove);
+    document.addEventListener("mouseup", mouseUp);
+  };
+
+  const mouseMove = (e) => {
+    //1 - Calculate move direction
+    let mouseMoveDir = {
+      x: mouseStartPos.x - e.clientX,
+      y: mouseStartPos.y - e.clientY,
+    };
+
+    //2 - Update start position for next move.
+    mouseStartPos.x = e.clientX;
+    mouseStartPos.y = e.clientY;
+
+    //3 - Update card top and left position.
+    setPositon({
+      x: cardRef.current.offsetLeft - mouseMoveDir.x,
+      y: cardRef.current.offsetTop - mouseMoveDir.y,
+    });
+  };
+
+  const mouseUp = () => {
+    document.removeEventListener("mousemove", mouseMove);
+    document.removeEventListener("mouseup", mouseUp);
+  };
+
   return (
     <div
+      ref={cardRef}
       className="card"
       style={{
         backgroundColor: colors.colorBody,
@@ -29,6 +66,7 @@ const NoteCard = ({ note }) => {
     >
       <div
         className="card-header"
+        onMouseDown={mouseDown}
         style={{ backgroundColor: colors.colorHeader }}
       >
         <Trash />
